5708e13ed8443f4646dd8515a88eadf8789a858e
Aliaksandr Hvozdzeu <aliaksandr.hvozdzeu@gmail.com>
2023-07-21 14:14:00 +0300

    Task #3: Add Draw menu

---
+++ src/components/customSidebarMenu/CustomSidebarMenu.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import {SafeAreaView, Text, Linking, View} from 'react-native';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+import {styles} from './styles';
+import {Icon} from 'react-native-elements';
+
+const CustomSidebarMenu = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text style={styles.menuLabel}>Ecommerce Store</Text>
+      <DrawerContentScrollView>
+        <Text style={styles.menuGroupText}>My Account</Text>
+        <DrawerItem
+          label="My Profile"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="font-awesome"
+              name="user"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <DrawerItem
+          label="My Wish List"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="antdesign"
+              name="heart"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <DrawerItem
+          label="My Cart"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="font-awesome"
+              name="shopping-cart"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <DrawerItem
+          label="My Orders"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="font-awesome"
+              name="cart-arrow-down"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <View style={styles.menuLine} />
+        <Text style={styles.menuGroupText}>Support</Text>
+        <DrawerItem
+          label="Email"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="material-community-icons"
+              name="mail"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <DrawerItem
+          label="Call"
+          icon={() => (
+            <Icon
+              style={styles.menuIcon}
+              type="ionicons"
+              name="call"
+              size={styles.menuIcon.size}
+              color={styles.menuIcon.color}
+            />
+          )}
+          labelStyle={styles.menuItem}
+          style={styles.drawItem}
+          onPress={() => Linking.openURL('https://aboutreact.com/')}
+        />
+        <View style={styles.menuLine} />
+        <View style={styles.share}>
+          <DrawerItem
+            label="Share"
+            icon={() => (
+              <Icon
+                style={styles.menuIcon}
+                type="entypo"
+                name="share"
+                size={styles.menuIcon.size}
+                color={styles.menuIcon.color}
+              />
+            )}
+            labelStyle={styles.menuItem}
+            style={styles.drawItem}
+            onPress={() => Linking.openURL('https://aboutreact.com/')}
+          />
+        </View>
+      </DrawerContentScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default CustomSidebarMenu;
---
+++ src/components/customSidebarMenu/styles.ts
@@ -0,0 +1,52 @@
+import {StyleSheet} from 'react-native';
+import {COLORS} from '../../utils/colors';
+
+export const styles = StyleSheet.create({
+  menuLabel: {
+    color: COLORS.blue_300,
+    fontFamily: 'Roboto',
+    fontSize: 40,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 50,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  menuGroupText: {
+    color: COLORS.neutral_500,
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    fontStyle: 'normal',
+    fontWeight: '700',
+    lineHeight: 25,
+    marginTop: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 5,
+  },
+  menuItem: {
+    color: COLORS.neutral_700,
+    fontFamily: 'Roboto',
+    fontSize: 15,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 25,
+    marginLeft: -10,
+  },
+  menuLine: {
+    borderBottomColor: COLORS.neutral_300,
+    borderBottomWidth: 1,
+    marginTop: 20,
+  },
+  share: {
+
+  },
+  menuIcon: {
+    color: COLORS.blue_500,
+    size: 20,
+  },
+  drawItem: {
+    marginBottom: -10,
+  },
+});
